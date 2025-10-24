d703fa638d83b995e8a4e00779839c84ca532654
Xinchi Zhang <yemmy0221@gmail.com>
2025-10-24 15:43:54 +0800

    Update URLs to point to new GitHub pages

--- worker.js
+++ worker.js
@@ -17,8 +17,8 @@ const protect_keylist = [
   "password",
 ]
 
-let index_html = "https://crazypeace.github.io/Url-Shorten-Worker/" + config.theme + "/index.html"
-let result_html = "https://crazypeace.github.io/Url-Shorten-Worker/" + config.theme + "/result.html"
+let index_html = "https://yemmy0221.github.io/Url-Shorten-Worker/" + config.theme + "/index.html"
+let result_html = "https://yemmy0221.github.io/Url-Shorten-Worker/" + config.theme + "/result.html"
 
 const html404 = `<!DOCTYPE html>
   <html>
